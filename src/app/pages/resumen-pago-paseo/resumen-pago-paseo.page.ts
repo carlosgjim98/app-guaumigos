14c938dda10940374059dca48d898d8adf437fd6
GassinDev <juangassinctic@gmail.com>
2024-10-18 14:59:26 +0200

    Pantalla 14 terminada

--- src/app/pages/resumen-pago-paseo/resumen-pago-paseo.page.ts
+++ src/app/pages/resumen-pago-paseo/resumen-pago-paseo.page.ts
@@ -29,6 +29,12 @@ export class ResumenPagoPaseoPage implements OnInit {
         imgUsuario: 'assets/imgs/usuarios/Rectángulo 19618.png',
         precio: '15€',
         duracion: '1 horas',
+        fecha: '18/10/2024',
+        hora: '11:00 A.M',
+        imgPerro: 'assets/imgs/perros/Rectángulo 19665.png',
+        nombrePerro: 'Moka',
+        raza: 'Pug',
+        edadPerro: '2'
       }
     ];
   }
