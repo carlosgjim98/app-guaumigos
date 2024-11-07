cfa08264f2ee6294404d2ad46c2c76b5385f4fd3
carlosgonzalez3 <carlosgonzalez@xerintel.es>
2024-11-07 14:00:15 +0100

    roles

--- src/app/pages/seleccionar-usuario/seleccionar-usuario.page.ts
+++ src/app/pages/seleccionar-usuario/seleccionar-usuario.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-seleccionar-usuario',
@@ -9,7 +10,7 @@ export class SeleccionarUsuarioPage {
 
   opcionSeleccionada: string = '';
 
-  //ESO SI LOS TIPOS SON FIJOS
+  
   iconos = {
     dueño: {
       normal: 'assets/icons/acceso-cliente.svg',
@@ -25,17 +26,33 @@ export class SeleccionarUsuarioPage {
     },
   };
 
+  constructor(private router: Router) {}
+
+  // Lógica para seleccionar una opción de usuario
   seleccionarOpcion(opcion: string) {
     this.opcionSeleccionada = opcion;
   }
 
+  // Verificar si la opción está seleccionada
   esSeleccionada(opcion: string): boolean {
     return this.opcionSeleccionada === opcion;
   }
 
+  // Obtener el icono correspondiente dependiendo si está seleccionado o no
   obtenerRutaIcono(opcion: string): string {
     return this.esSeleccionada(opcion)
       ? this.iconos[opcion].seleccionado
       : this.iconos[opcion].normal;
+  }
+
+  // Al enviar el formulario, guardar el tipo de usuario y redirigir al login
+  continuar() {
+    if (this.opcionSeleccionada) {
+      // Guardar el tipo de usuario seleccionado en sessionStorage
+      sessionStorage.setItem('tipoUsuario', this.opcionSeleccionada);
+
+      // Redirigir al login
+      this.router.navigate(['/login']);
+    }
   }
 }
