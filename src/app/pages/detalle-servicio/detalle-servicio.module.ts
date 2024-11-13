ec864b8b5a8e57eb6b0bae4b630a349863262603
carlosgonzalez3 <carlosgonzalez@xerintel.es>
2024-11-13 11:08:46 +0100

    cambios

--- src/app/pages/detalle-servicio/detalle-servicio.module.ts
+++ src/app/pages/detalle-servicio/detalle-servicio.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { DetalleServicioPageRoutingModule } from './detalle-servicio-routing.module';
-
+import { FullCalendarModule } from '@fullcalendar/angular';
 import { DetalleServicioPage } from './detalle-servicio.page';
 
 @NgModule({
@@ -13,6 +13,7 @@ import { DetalleServicioPage } from './detalle-servicio.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    FullCalendarModule,
     DetalleServicioPageRoutingModule
   ],
   declarations: [DetalleServicioPage]
